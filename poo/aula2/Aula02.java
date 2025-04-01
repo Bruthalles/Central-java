4970a3d69e82efe7294ee094f3dad3f090361527
Bruthalles <thallesgmkr@gmail.com>
2025-04-01 09:28:41 -0300

    started class Caneta

---
+++ poo/aula2/Aula02.java
@@ -0,0 +1,7 @@
+package poo.aula2;
+
+public class Aula02{
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ poo/aula2/Caneta.java
@@ -0,0 +1,13 @@
+package poo.aula2;
+
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+
+    void rabiscar(){}
+    void tampar(){}
+    void destampar(){}
+}
