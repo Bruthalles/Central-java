9a948f94dfb6d649157712c3e25c9d18e39daf4d
Bruthalles <thallesgmkr@gmail.com>
2025-04-01 08:08:26 -0300

    identação adequada

--- exemplo1/PedidoFisico.java
+++
@@ -1,9 +0,0 @@
-package exemplo1;
-
-public class PedidoFisico extends Pedido{
-    @Override
-    protected PedidoItem criarItem(Produto produto, int quantidade){
-        return new ItemPedidoLojaFisica(produto, quantidade);
-        
-    }
-}
--- exemplo1/PedidoItem.java
+++
@@ -1,7 +0,0 @@
-package exemplo1;
-
-public interface PedidoItem{
-    Produto getProduto();
-    int getQuantidade();
-    double getPreco();
-}
