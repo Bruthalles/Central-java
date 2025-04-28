ddfbd233db8ce93ab18a75f8298a7cae44cae98e
Bruthalles <thallesgmkr@gmail.com>
2025-04-28 20:03:53 -0300

    aula7 flyweight

---
+++ padroes_de_projetos/aula7/facade/LojaFacade.java
@@ -0,0 +1,11 @@
+package padroes_de_projetos.aula7.facade;
+
+public class LojaFacade{
+    private Estoque estoque;
+    private Pagamento pagamento;
+    private Envio envio;
+
+    public void comprarFilme(){
+
+    }
+}
